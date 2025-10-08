b5822df9d196f199d0c9d59ab29dfcf582c766d1
Hansika-Prabodini <hansika@turintech.ai>
2025-10-08 12:53:13 +0530

    delete data.R file

--- R/R/data.R
+++
@@ -1,8 +0,0 @@
-# added new commnents to explain the purpoose of this testing
-# Modified from original file in prophet package
-# --------------------------------------------------------------    
-#'
-#' @format A data frame with four variables: ds, holiday, country, year
-#' @source \url{https://github.com/facebook/prophet/blob/main/python/scripts/generate_holidays_file.py}
-#' @export
-"generated_holidays"

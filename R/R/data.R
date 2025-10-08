cafe931368e1864bb9b4c0532202d72c6e6f86cd
Hansika-Prabodini <hansika@turintech.ai>
2025-10-08 12:14:42 +0530

    added changes in R file

--- R/R/data.R
+++ R/R/data.R
@@ -1,12 +1,6 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-
-#' Generated table of holiday dates at the country level from 1995 to 2045
-#'
-#' The data is primarily based on the Python package [holidays](https://pypi.org/project/holidays/)
+# added new commnents to explain the purpoose of this testing
+# Modified from original file in prophet package
+# --------------------------------------------------------------    
 #'
 #' @format A data frame with four variables: ds, holiday, country, year
 #' @source \url{https://github.com/facebook/prophet/blob/main/python/scripts/generate_holidays_file.py}

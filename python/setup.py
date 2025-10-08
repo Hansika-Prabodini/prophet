af98a005b996e978d3674c643a030d2a40d52d55
Hansika-Prabodini <hansika@turintech.ai>
2025-10-08 10:52:59 +0530

    added some chnages to setup.py file for testing purposes

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# Changes from VS Code template:
+# Added some commnts lines to explain functions
+# Added error handling and logging to build_cmdstan_model function  
+# --------------------------------------------------------------
 
 import logging
 import os
